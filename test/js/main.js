ed7fac851eed628a61482491fc798d2873f89cb9
Benny214 <benny.moralez@gmail.com>
2015-02-18 18:52:08 +0300

    done

--- test/js/main.js
+++ test/js/main.js
@@ -1,1 +1,16 @@
+$(document).ready(function() {
+	$(function() {
+		$('.user__item').each(function(i) {
+			$(this).delay((i++) * 500).fadeTo(1000, 1);
+		})
+	});
 
+	$("#group2").click(function() {
+		$(".group2").toggle(300);
+	});
+	$("#group1").click(function() {
+		$(".group1").toggle(300);
+	});
+
+
+});
